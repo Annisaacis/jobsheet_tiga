c50d1d40bb48c4e35ed4e82020a54eaea7af5b5e
= <=>
2021-03-08 00:59:00 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/jobsheet_tiga/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jobsheet_tiga
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Konverter Suhu"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(8),
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Masukkan Suhu Dalam Celcius',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  //variabel berubah
+  double _inputUser = 0;
+  double _kelvin = 0;
+  //tambahkan variabel lain yang dibutuhkan
+  var kelvin;
+}
